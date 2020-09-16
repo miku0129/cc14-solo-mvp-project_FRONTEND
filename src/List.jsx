0a12a6d09afdbdb91c4c407f17f83f88c11e9c13
miku <svartkatt39@gmail.com>
2020-09-16 14:10:30 +0900

    all restaurant logic works

--- src/List.jsx
+++ src/List.jsx
@@ -22,7 +22,7 @@ function List() {
     console.log(data);
     // setState(data[0]);
     const all = data.map((el) => {
-      return el.name + " : " + el.feature + " / ";
+      return el.name + ":" + el.feature + "/";
     });
     setAllRestaurants(all);
   }
@@ -42,17 +42,89 @@ function List() {
     const res = await req;
     const data = res.data;
     // console.log(data);
+    // if (value === "shing") {
+    //   // console.log(data[0]);
+    //   let temp = [];
+    //   let info = temp.concat([
+    //     data[0].name,
+    //     "/",
+    //     data[0].feature,
+    //     "/",
+    //     data[0].business_hours1,
+    //     "/",
+    //     data[0].business_hours2,
+    //     ",",
+    //     data[0].regular_holiday,
+    //     ",",
+    //     data[0].place,
+    //     ",",
+    //     data[0].tell,
+    //     ",",
+    //     data[0].url,
+    //   ]);
+    //   setRestaurant(info);
+    // } else if (value === "momo") {
+    //   let temp = [];
+    //   let info = temp.concat([
+    //     data[1].name,
+    //     "/",
+    //     data[1].feature,
+    //     "/",
+    //     data[1].business_hours1,
+    //     "/",
+    //     data[1].business_hours2,
+    //     ",",
+    //     data[1].regular_holiday,
+    //     ",",
+    //     data[1].place,
+    //     ",",
+    //     data[1].tell,
+    //     ",",
+    //     data[1].url,
+    //   ]);
+    //   setRestaurant(info);
+    // }
+
+    let i = 0;
     if (value === "shing") {
-      // console.log(data[0]);
-      let temp = [];
-      let info = temp.concat([data[0].name, data[0].feature, data[0].tell]);
-      setRestaurant(info);
+      i = 0;
     } else if (value === "momo") {
-      console.log(data[1]);
-      let temp = [];
-      let info = temp.concat([data[1].name, data[1].feature, data[1].tell]);
-      setRestaurant(info);
+      i = 1;
+    } else if (value === "サラム") {
+      i = 2;
+    } else if (value === "タマン") {
+      i = 3;
+    } else if (value === "NAWAB") {
+      i = 4;
+    } else if (value === "8") {
+      i = 5;
+    } else if (value === "AANGAN") {
+      i = 6;
+    } else if (value === "NI") {
+      i = 7;
+    } else if (value === "Atila") {
+      i = 8;
     }
+    // console.log(data[0]);
+    let temp = [];
+    let info = temp.concat([
+      data[i].name,
+      "/",
+      data[i].feature,
+      "/",
+      data[i].business_hours1,
+      "/",
+      data[i].business_hours2,
+      ",",
+      data[i].regular_holiday,
+      ",",
+      data[i].place,
+      ",",
+      data[i].tell,
+      ",",
+      // data[i].url,
+    ]);
+    setRestaurant(info);
   }
   console.log(restaurantInfo);
 
@@ -60,8 +132,8 @@ function List() {
     <div className="list">
       {/* feature : I want to make the list more dinamic  */}
       <ul>
-        <li>All restaurants Feature?</li>
-        <button onClick={getAllData}>Info</button>
+        {/* <li>All restaurants Feature?</li>
+        <button onClick={getAllData}>Info</button> */}
         <li>シンガポール海南鶏飯</li>
         <button onClick={restaurantFunc} value="shing">
           Info
@@ -70,20 +142,34 @@ function List() {
         <button onClick={restaurantFunc} value="momo">
           Info
         </button>
-        {/* <li>サラムナマステ</li>
-        <button>Info</button>
+        <li>サラムナマステ</li>
+        <button onClick={restaurantFunc} value="サラム">
+          Info
+        </button>
         <li>タマンのカレー屋さん</li>
-        <button>Info</button>
+        <button onClick={restaurantFunc} value="タマン">
+          Info
+        </button>
         <li>NAWAB</li>
-        <button>Info</button>
+        <button onClick={restaurantFunc} value="NAWABU">
+          Info
+        </button>
         <li>Chinese cafe 8</li>
-        <button>Info</button>
+        <button onClick={restaurantFunc} value="8">
+          Info
+        </button>
         <li>AANGAN</li>
-        <button>Info</button>
+        <button onClick={restaurantFunc} value="AANGAN">
+          Info
+        </button>
         <li>Nong Inlay</li>
-        <button>Info</button>
+        <button onClick={restaurantFunc} value="NI">
+          Info
+        </button>
         <li>Atila's hundmade curry</li>
-        <button>Info</button> */}
+        <button onClick={restaurantFunc} value="Atila">
+          Info
+        </button>
       </ul>
       <Info
         allRestaurants={allRestaurantsInfo}
